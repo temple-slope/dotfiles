6769ef3908b0633d71100cf9d25306e5eec79278
temple-slope <kazuma.m.h.y@gmail.com>
2025-10-08 09:12:23 +0900

    Refactor Zsh setup script, update Karabiner configuration for better keybindings, and modify VSCode terminal settings

--- run_once_setup_local.sh
+++ run_once_setup_local.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
 # ~/.config/zsh/local.zsh を自動生成する（初回のみ）
-cat <<EOF > "{{ .chezmoi.homeDir }}/.config/zsh/local.zsh"
-# local zsh settings (not under Git control)
-# You can customize this per machine
-
-# example:
-# export ZSH_THEME="powerlevel10k/powerlevel10k"
-EOF
+cat <<EOF > "$HOME/.config/zsh/local.zsh"
