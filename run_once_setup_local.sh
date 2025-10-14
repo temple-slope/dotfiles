b6d475538fe0f8f49303730e38a6aacf6d6ecd2d
temple slope <kazuma.m.h.y@gmail.com>
2025-10-14 10:39:39 +0900

    Fix here-document in setup local script

--- run_once_setup_local.sh
+++ run_once_setup_local.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-
 # ~/.config/zsh/local.zsh を自動生成する（初回のみ）
-cat <<EOF > "$HOME/.config/zsh/local.zsh"
+set -eu
+
+mkdir -p "$HOME/.config/zsh"
+
+cat <<'LOCAL_ZSH' > "$HOME/.config/zsh/local.zsh"
+# ~/.config/zsh/local.zsh
+# このファイルには個別の設定を記述してください。
+# 例:
+# export EDITOR="nvim"
+# alias ll='ls -alF'
+
+LOCAL_ZSH
